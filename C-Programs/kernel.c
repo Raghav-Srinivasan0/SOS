befb091b47af9474248ed1577fc1c13993d42180
Raghav-Srinivasan0 <96836278+Raghav-Srinivasan0@users.noreply.github.com>
2023-07-01 16:43:47 -0400

    Update kernel.c

--- C-Programs/kernel.c
+++ C-Programs/kernel.c
@@ -1,91 +1,5 @@
-#ifndef VGA_WIDTH
-#define VGA_WIDTH 80
-#endif
-
-#ifndef VGA_HEIGHT
-#define VGA_HEIGHT 25
-#endif
-
-#define PIC1_COMMAND_PORT 0x20
-#define PIC1_DATA_PORT 0x21
-#define PIC2_COMMAND_PORT 0xA0
-#define PIC2_DATA_PORT 0xA1
-#define KEYBOARD_DATA_PORT 0x60
-#define KEYBOARD_STATUS_PORT 0x64
-
-#define IDT_SIZE 256
-#define KERNEL_CODE_SEGMENT_OFFSET 0x8
-#define IDT_INTERRUPT_GATE_32BIT 0x8e
-
-#define BLACK 0
-#define BLUE 1
-#define GREEN 2
-#define CYAN 3
-#define RED 4
-#define MAGENTA 5
-#define BROWN 6
-#define LIGHTGRAY 7
-#define DARKGRAY 8
-#define LIGHTBLUE 9
-#define LIGHTGREEN 10
-#define LIGHTCYAN 11
-#define LIGHTRED 12
-#define LIGHTMAGENTA 13
-#define YELLOW 13
-#define WHITE 15
-
-#include <stdint.h>
-#include "keyboard_map.h"
-//#include "disk.h"
-
-#include <stddef.h>
-#include <stdbool.h>
-#include <stdarg.h>
-#include "multiboot.h"
-
-void print(char* string);
-void safe_println(char* string, int len);
-void safe_print(char* string, int len);
-void printchar(char c);
-void printchar_at(char c, int row, int col);
-extern void print_char_with_asm(char c, int row, int col);
-void clear_screen();
-void print_prompt();
-void print_message();
-void newline();
-void backspace();
-
-// IDT (from: https://www.youtube.com/watch?v=YtnNX074jMU&list=PL3Kz_hCNpKSTFCTJtP4-9mkYDVM7rAprW&index=12)
-
-struct IDT_pointer {
-	unsigned short limit;
-	unsigned int base;
-} __attribute__((packed));
-
-struct IDT_entry {
-	unsigned short offset_lowerbits;
-	unsigned short selector;
-	unsigned char zero;
-	unsigned char type_attr;
-	unsigned short offset_upperbits;
-} __attribute__((packed));
-
-extern void print_char_with_asm(char c, int row, int col);
-extern void load_gdt();
-extern void keyboard_handler();
-extern char ioport_in(unsigned short port);
-extern void ioport_out(unsigned short port, unsigned char data);
-extern void load_idt(struct IDT_pointer* idt_address);
-extern void enable_interrupts();
-extern void cmos_read(unsigned char index, unsigned char tvalue);
-extern void cmos_write(unsigned char index, unsigned char tvalue);
-extern void TimerIRQ(uint32_t countdown);
-extern void floppy_init();
-extern void floppy_datarate(uint8_t datarate);
-extern char floppy_dir();
-extern void floppy_fifo(uint8_t command);
-
-struct IDT_entry IDT[IDT_SIZE];
+#include "kernel.h"
+#include "ide.h"
 
 //Keyboard Stuff
 
@@ -94,7 +8,17 @@ unsigned char inPortB (unsigned short _port)
 	return ioport_in(_port);
 }
 
+unsigned char inb (unsigned short _port)
+{
+	return ioport_in(_port);
+}
+
 void outPortB (unsigned short _port, unsigned char _data)
+{
+	ioport_out(_port,_data);
+}
+
+void outb (unsigned short _port, unsigned char _data)
 {
 	ioport_out(_port,_data);
 }
@@ -1062,13 +986,234 @@ void handle_keyboard_interrupt()
 	}
 }
 
+/* IDE Drive Stuff */
+
+unsigned char ide_read(unsigned char channel, unsigned char reg) {
+   unsigned char result;
+   if (reg > 0x07 && reg < 0x0C)
+      ide_write(channel, ATA_REG_CONTROL, 0x80 | channels[channel].nIEN);
+   if (reg < 0x08)
+      result = inb(channels[channel].base + reg - 0x00);
+   else if (reg < 0x0C)
+      result = inb(channels[channel].base  + reg - 0x06);
+   else if (reg < 0x0E)
+      result = inb(channels[channel].ctrl  + reg - 0x0A);
+   else if (reg < 0x16)
+      result = inb(channels[channel].bmide + reg - 0x0E);
+   if (reg > 0x07 && reg < 0x0C)
+      ide_write(channel, ATA_REG_CONTROL, channels[channel].nIEN);
+   return result;
+}
+
+void ide_write(unsigned char channel, unsigned char reg, unsigned char data) {
+   if (reg > 0x07 && reg < 0x0C)
+      ide_write(channel, ATA_REG_CONTROL, 0x80 | channels[channel].nIEN);
+   if (reg < 0x08)
+      outb(channels[channel].base  + reg - 0x00, data);
+   else if (reg < 0x0C)
+      outb(channels[channel].base  + reg - 0x06, data);
+   else if (reg < 0x0E)
+      outb(channels[channel].ctrl  + reg - 0x0A, data);
+   else if (reg < 0x16)
+      outb(channels[channel].bmide + reg - 0x0E, data);
+   if (reg > 0x07 && reg < 0x0C)
+      ide_write(channel, ATA_REG_CONTROL, channels[channel].nIEN);
+}
+
+unsigned char ide_polling(unsigned char channel, unsigned int advanced_check) {
+ 
+   // (I) Delay 400 nanosecond for BSY to be set:
+   // -------------------------------------------------
+   for(int i = 0; i < 4; i++)
+      ide_read(channel, ATA_REG_ALTSTATUS); // Reading the Alternate Status port wastes 100ns; loop four times.
+ 
+   // (II) Wait for BSY to be cleared:
+   // -------------------------------------------------
+   while (ide_read(channel, ATA_REG_STATUS) & ATA_SR_BSY)
+      ; // Wait for BSY to be zero.
+ 
+   if (advanced_check) {
+      unsigned char state = ide_read(channel, ATA_REG_STATUS); // Read Status Register.
+ 
+      // (III) Check For Errors:
+      // -------------------------------------------------
+      if (state & ATA_SR_ERR)
+         return 2; // Error.
+ 
+      // (IV) Check If Device fault:
+      // -------------------------------------------------
+      if (state & ATA_SR_DF)
+         return 1; // Device Fault.
+ 
+      // (V) Check DRQ:
+      // -------------------------------------------------
+      // BSY = 0; DF = 0; ERR = 0 so we should check for DRQ now.
+      if ((state & ATA_SR_DRQ) == 0)
+         return 3; // DRQ should be set
+ 
+   }
+ 
+   return 0; // No Error.
+ 
+}
+
+/* Hacky Sleep */
+
+void sleep(int millis)
+{
+    for (int i = 0; i<millis*1000000; i++)
+    {
+        continue;
+    }
+    return;
+}
+
+static inline long inl(uint16_t port)
+{
+    long ret;
+    asm volatile ( "inl %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port)
+                   : "memory");
+    return ret;
+}
+
+void insl(unsigned reg, unsigned int *buffer, int quads)
+{
+    int index;
+    for(index = 0; index < quads; index++)
+    {
+        buffer[index] = inl(reg);
+    }
+}
+
+void ide_read_buffer(unsigned char channel, unsigned char reg, unsigned int * buffer,
+                     unsigned int quads) {
+   /* WARNING: This code contains a serious bug. The inline assembly trashes ES and
+    *           ESP for all of the code the compiler generates between the inline
+    *           assembly blocks.
+    */
+   if (reg > 0x07 && reg < 0x0C)
+      ide_write(channel, ATA_REG_CONTROL, 0x80 | channels[channel].nIEN);
+   asm("pushw %es; movw %ds, %ax; movw %ax, %es");
+   if (reg < 0x08)
+      insl(channels[channel].base  + reg - 0x00, buffer, quads);
+   else if (reg < 0x0C)
+      insl(channels[channel].base  + reg - 0x06, buffer, quads);
+   else if (reg < 0x0E)
+      insl(channels[channel].ctrl  + reg - 0x0A, buffer, quads);
+   else if (reg < 0x16)
+      insl(channels[channel].bmide + reg - 0x0E, buffer, quads);
+   asm("popw %es;");
+   if (reg > 0x07 && reg < 0x0C)
+      ide_write(channel, ATA_REG_CONTROL, channels[channel].nIEN);
+}
+
+void ide_initialize(unsigned int BAR0, unsigned int BAR1, unsigned int BAR2, unsigned int BAR3,
+unsigned int BAR4) {
+ 
+   int j, k, count = 0;
+ 
+   // 1- Detect I/O Ports which interface IDE Controller:
+   channels[ATA_PRIMARY  ].base  = (BAR0 & 0xFFFFFFFC) + 0x1F0 * (!BAR0);
+   channels[ATA_PRIMARY  ].ctrl  = (BAR1 & 0xFFFFFFFC) + 0x3F6 * (!BAR1);
+   channels[ATA_SECONDARY].base  = (BAR2 & 0xFFFFFFFC) + 0x170 * (!BAR2);
+   channels[ATA_SECONDARY].ctrl  = (BAR3 & 0xFFFFFFFC) + 0x376 * (!BAR3);
+   channels[ATA_PRIMARY  ].bmide = (BAR4 & 0xFFFFFFFC) + 0; // Bus Master IDE
+   channels[ATA_SECONDARY].bmide = (BAR4 & 0xFFFFFFFC) + 8; // Bus Master IDE
+    // 2- Disable IRQs:
+   ide_write(ATA_PRIMARY  , ATA_REG_CONTROL, 2);
+   ide_write(ATA_SECONDARY, ATA_REG_CONTROL, 2);
+
+   // 3- Detect ATA-ATAPI Devices:
+   for (int i = 0; i < 2; i++)
+      for (j = 0; j < 2; j++) {
+        unsigned char err = 0, type = IDE_ATA, status;
+        ide_devices[count].Reserved = 0; // Assuming that no drive here.
+ 
+        // (I) Select Drive:
+        ide_write(i, ATA_REG_HDDEVSEL, 0xA0 | (j << 4)); // Select Drive.
+        sleep(1); // Wait 1ms for drive select to work.
+ 
+        // (II) Send ATA Identify Command:
+        ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
+        sleep(1); // This function should be implemented in your OS. which waits for 1 ms.
+                 // it is based on System Timer Device Driver.
+ 
+        // (III) Polling:
+        if (ide_read(i, ATA_REG_STATUS) == 0) continue; // If Status = 0, No Device.
+ 
+        while(1) {
+           status = ide_read(i, ATA_REG_STATUS);
+           if ((status & ATA_SR_ERR)) {err = 1; break;} // If Err, Device is not ATA.
+           if (!(status & ATA_SR_BSY) && (status & ATA_SR_DRQ)) break; // Everything is right.
+        }
+ 
+        // (IV) Probe for ATAPI Devices:
+ 
+        if (err != 0) {
+           unsigned char cl = ide_read(i, ATA_REG_LBA1);
+           unsigned char ch = ide_read(i, ATA_REG_LBA2);
+ 
+            if (cl == 0x14 && ch ==0xEB)
+               type = IDE_ATAPI;
+            else if (cl == 0x69 && ch == 0x96)
+               type = IDE_ATAPI;
+            else
+               continue; // Unknown Type (may not be a device).
+ 
+            ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY_PACKET);
+            sleep(1);
+         }
+ 
+         // (V) Read Identification Space of the Device:
+         ide_read_buffer(i, ATA_REG_DATA, (unsigned int *) ide_buf, 128);
+ 
+         // (VI) Read Device Parameters:
+         ide_devices[count].Reserved     = 1;
+         ide_devices[count].Type         = type;
+         ide_devices[count].Channel      = i;
+         ide_devices[count].Drive        = j;
+         ide_devices[count].Signature    = *((unsigned short *)(ide_buf + ATA_IDENT_DEVICETYPE));
+         ide_devices[count].Capabilities = *((unsigned short *)(ide_buf + ATA_IDENT_CAPABILITIES));
+         ide_devices[count].CommandSets  = *((unsigned int *)(ide_buf + ATA_IDENT_COMMANDSETS));
+ 
+         // (VII) Get Size:
+         if (ide_devices[count].CommandSets & (1 << 26))
+            // Device uses 48-Bit Addressing:
+            ide_devices[count].Size   = *((unsigned int *)(ide_buf + ATA_IDENT_MAX_LBA_EXT));
+         else
+            // Device uses CHS or 28-bit Addressing:
+            ide_devices[count].Size   = *((unsigned int *)(ide_buf + ATA_IDENT_MAX_LBA));
+ 
+         // (VIII) String indicates model of device (like Western Digital HDD and SONY DVD-RW...):
+         for(k = 0; k < 40; k += 2) {
+            ide_devices[count].Model[k] = ide_buf[ATA_IDENT_MODEL + k + 1];
+            ide_devices[count].Model[k + 1] = ide_buf[ATA_IDENT_MODEL + k];}
+         ide_devices[count].Model[40] = 0; // Terminate String.
+ 
+        count++;
+    }
+ 
+   // 4- Print Summary:
+   for (int i = 0; i < 4; i++)
+        if (ide_devices[i].Reserved == 1) {
+            printf(" Found %s Drive %dGB - %s\n", (const char *[]){"ATA", "ATAPI"}[ide_devices[i].Type],         /* Type */
+            ide_devices[i].Size / 1024 / 1024 / 2,               /* Size */
+            ide_devices[i].Model);
+        }
+}
+
+/* AHCI Stuff */
+
 void kernel_main(multiboot_info_t* mbd, uint32_t magic)
 {
 	enable_cursor(0,15);
 	currentColor = color(LIGHTGREEN,CYAN);
 	print_init();
 	currentColor = defaultColor;
-	multiboot_init(mbd,magic);
+	//multiboot_init(mbd,magic);
+    ide_initialize(0x1F0, 0x3F6, 0x170, 0x376, 0x000);
 	init_idt();
 	kb_init();
 	enable_interrupts();
